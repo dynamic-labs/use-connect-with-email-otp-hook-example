7937963b5fab9fa75b816aed15f72924973adb6a
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2023-12-21 12:12:54 -0300

    adds useConnectWithEmailOtp example

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,79 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { FormEventHandler, useState } from "react";
+
+import {
+  DynamicWidget,
+  useConnectWithEmailOtp,
+  useDynamicContext,
+} from "@dynamic-labs/sdk-react-core";
+
+export const EmailLogin = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState<boolean>(false);
+  const { connectWithEmail, verifyOneTimePassword } = useConnectWithEmailOtp({
+    provider: "magicLink",
+  });
+
+  const onSubmitEmailHandler: FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+
+    const email = event.currentTarget.email.value;
+
+    await connectWithEmail(email);
+
+    setEmailSubmitted(true);
+  };
+
+  const onSubmitOtpHandler: FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+
+    const otp = event.currentTarget.otp.value;
+
+    await verifyOneTimePassword(otp);
+
+    setEmailSubmitted(false);
+  };
+
+  if (emailSubmitted) {
+    return (
+      <form key="otp-form" onSubmit={onSubmitOtpHandler}>
+        <input type="text" name="otp" placeholder="OTP" />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+
+  return (
+    <form key="email-form" onSubmit={onSubmitEmailHandler}>
+      <input type="email" name="email" placeholder="Email" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const LoggedInUser = () => {
+  const { user } = useDynamicContext();
+
+  return (
+    <>
+      <DynamicWidget />
+      <p>user</p>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </>
+  );
+};
 
 export default function Home() {
+  const { user } = useDynamicContext();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <>
+      {!user && <EmailLogin />}
+      {!!user && <LoggedInUser />}
+    </>
+  );
 }
